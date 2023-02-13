7553fc4da29e8bb59489ee6c88bc2c25caf92f9e
kumawat30rahul <kumawat30rahul@gmail.com>
2023-02-13 13:59:30 +0530

    ghj

--- script.js
+++ script.js
@@ -9,31 +9,48 @@ let ageButton = document.getElementById('AGE')
 let emailButton = document.getElementById('EMAIL')
 let phoneButton = document.getElementById('PHONE')
 let gotData = false;
-getUser.addEventListener("click",() => {
-    heading.innerText = ""
-    fetch('https://randomuser.me/api/')
+
+
+fetch('https://randomuser.me/api/')
         .then(response => response.json())
         .then((data) => {
             let userData = data.results
             gotData = true;
             let x = 0
-            userData.map(element => {
-                console.log(userData[x]);
-                userImage.src = userData[x].picture.large
-                userName.innerText = userData[x].name.first + " " + userData[x].name.last
+            userImage.src = userData[x].picture.large
+            userName.innerText = userData[x].name.first + " " + userData[x].name.last
 
-                if(gotData){
-                    ageButton.addEventListener("click",() => {
-                        heading.innerText = userData[x].dob.age
-                    })
-                    emailButton.addEventListener("click",() => {
-                        heading.innerText = userData[x].email
-                    })
-                    phoneButton.addEventListener("click",() => {
-                        heading.innerText = userData[x].phone
-                    })
-                }
+            ageButton.addEventListener("click",() => {
+                heading.innerText = userData[x].dob.age
+            })
+            emailButton.addEventListener("click",() => {
+                heading.innerText = userData[x].email
+            })
+            phoneButton.addEventListener("click",() => {
+                heading.innerText = userData[x].phone
+            })
+
+            getUser.addEventListener("click",() => {
+                heading.innerText = ""
+                fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((data) => {
+            let userData = data.results
+            gotData = true;
+            let x = 0
+            userImage.src = userData[x].picture.large
+            userName.innerText = userData[x].name.first + " " + userData[x].name.last
+
+            ageButton.addEventListener("click",() => {
+                heading.innerText = userData[x].dob.age
+            })
+            emailButton.addEventListener("click",() => {
+                heading.innerText = userData[x].email
+            })
+            phoneButton.addEventListener("click",() => {
+                heading.innerText = userData[x].phone
             })
         })
-})
-
+            })
+            
+        })
